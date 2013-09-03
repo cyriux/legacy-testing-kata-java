d48155248b09fb47af6bfc306b5c6a5e5d37a3e9
cyriux <cyrillemartraire@gmail.com>
2013-09-03 23:08:40 +0200

    Deleted duplicate files

--- legacy-testing-kata-java/src/main/java/com/arolla/legacy/testing/quotebot/Application.java
+++
@@ -1,9 +0,0 @@
-package com.arolla.legacy.testing.quotebot;
-
-public class Application {
-	public static void main(String[] args) {
-		AutomaticQuoteBot bot = new AutomaticQuoteBot();
-		bot.sendAllQuotes("FAST");
-	}
-
-}
